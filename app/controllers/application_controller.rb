4dff90ad5c2626591dfe12ec7a49cd5454319228
大高俊輝 <to.so.lo.1205@gmail.com>
2017-02-19 23:34:52 +0900

    revise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery with: :exception
+  
+  before_action :authenticate_user!
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   private
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_updaet, keys: [:name])
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,16 @@
 class UsersController < ApplicationController
   
   def edit
-    @user = User.find(current_user.id)
   end
 
   def update
-    @user = User.find(current_user.id)
-    binding.pry
-    @user.update(update_params)
-    redirect_to controller: "messages", action: "index"
+    current_user.update(update_params)
+    redirect_to root_path
   end
 
   private
   def update_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
