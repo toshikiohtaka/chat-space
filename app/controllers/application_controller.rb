9a1b91a2d58f178f8b5e65f8a741af43859fa753
大高俊輝 <to.so.lo.1205@gmail.com>
2017-02-19 22:26:24 +0900

    make edit-view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,7 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_updaet, keys: [:name])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,18 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(current_user.id)
-    binding.pry
   end
 
   def update
+    @user = User.find(current_user.id)
+    binding.pry
+    @user.update(update_params)
+    redirect_to controller: "messages", action: "index"
+  end
 
+  private
+  def update_params
+    params.require(:user).permit(:name)
   end
 
 end
