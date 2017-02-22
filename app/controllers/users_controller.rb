ca996f5e94e6fb568fec79d6e31faa6a6b999cb9
大高俊輝 <to.so.lo.1205@gmail.com>
2017-02-22 19:32:29 +0900

    make flash msg & revise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
   
   def edit
+
   end
 
   def update
     current_user.update(update_params)
+    # binding.pry
     redirect_to root_path
   end
 
   private
   def update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resource :user,　only: [:edit, :update]
+  #resource :user,　only: [:edit, :update]
 end
