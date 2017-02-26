6b441db658b3b7e52de21d3af44cbd6a1427867d
大高俊輝 <to.so.lo.1205@gmail.com>
2017-02-26 21:28:54 +0900

    make group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,21 @@
 class GroupsController < ApplicationController
+  
+  def index
+    @groups = Group.all.order("created_at DESC")
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(name: group_params[:name])
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
