efe74d3c85da81f0b4f4b8c76682cb4c0af1641b
大高俊輝 <to.so.lo.1205@gmail.com>
2017-03-02 18:24:42 +0900

    revise 5

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,8 @@ class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
     @groups = current_user.groups.order("created_at DESC")
+    @members = @group.users
+    # binding.pry
   end
   
 end
