8ab81e503b09f21630d08b9d18e2dd650cbf1022
大高俊輝 <to.so.lo.1205@gmail.com>
2017-03-02 10:59:07 +0900

    revise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # validates :name, presence: true
+
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
