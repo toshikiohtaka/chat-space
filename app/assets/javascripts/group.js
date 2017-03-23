2562e679635b48a31bc77b17df864cceca4c2327
大高俊輝 <to.so.lo.1205@gmail.com>
2017-03-23 19:02:54 +0900

    revise group.js

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -4,22 +4,26 @@ $(document).on('turbolinks:load', function () {
 
   function addUser(user) {
     var html = `<li class="chat-group-user"> ${user.name}
-                  <a class="add-user-btn" data-user-name="${user.name}" data-user-id="${user.id}"> 追加 </a>
+                  <a class="add-user--btn" data-user-name="${user.name}" data-user-id="${user.id}"> 追加 </a>
                 </li>`;
     $('#chat-group-members').append(html);
   }
 
-  function addUserToGroup(name,id) {
+  function addUserToGroup(user) {
+    var name = user.find('a').data('user-name');
+    var id   = user.find('a').data('user-id');
     var html = `<li class="chat-group-added-user"> ${name}
                   <input name="group[user_ids][]" value="${id}" type="hidden">
-                  <a class="delete-user-btn" data-user-name="${name}" data-user-id="${id}"> 削除 </a>
+                  <a class="delete-user--btn" data-user-id="${id}"> 削除 </a>
                 </li>`;
     $('#chat-group-users').append(html);
     user_ids.push(id);
+    console.log(user_ids);
   }
 
-  function deleteUserId(id, user) {
-    var num = user_ids.indexOf(id);
+  function deleteUserId(user) {
+    var user_id = user.find('a').data('user-id');
+    var num     = user_ids.indexOf(user_id);
     user_ids.splice(num,1);
     console.log(user_ids);
     user.remove();
@@ -27,23 +31,19 @@ $(document).on('turbolinks:load', function () {
 
   $('#chat_group_names').on('keyup', function() {
     var preWord = '';
-    var input = $('#chat_group_names').val();
+    var input   = $('#chat_group_names').val();
     if(input == preWord) {
-      console.log('ajax実行しない');
       $('.chat-group-user').remove();
     } else {
-      console.log('ajax実行する');
       $('.chat-group-user').remove();
       $.ajax({
         type: 'GET',
-        url: '/users.json',
+        url: '/users',
         data: {
           user: input
-        },
-        dataType: 'json'
+        }
       })
       .done(function(data) {
-        console.log(data);
         $.each(data, function(i,user) {
           if(user_ids.indexOf(user.id) == -1) {
             addUser(user);
@@ -55,24 +55,18 @@ $(document).on('turbolinks:load', function () {
       });
     }
   });
-  $('#chat-group-members').on('click','.add-user-btn',function(e) {
-    console.log('追加');
+  $('#chat-group-members').on('click','.add-user--btn',function(e) {
     e.preventDefault
-    var user_parent = $(this).parent();
-    var user = user_parent.find('a');
-    var user_name = user.data('user-name');
-    var user_id = user.data('user-id');
-    if(user_ids.indexOf(user_id) == -1) {
-      user_parent.remove();
-      addUserToGroup(user_name,user_id);
+    var user = $(this).parent();
+    var id   = user.find('a').data('user-id');
+    if(user_ids.indexOf(id) == -1) {
+      user.remove();
+      addUserToGroup(user);
     }
   });
-  $('#chat-group-users').on('click','.delete-user-btn', function(e) {
-    console.log('削除');
+  $('#chat-group-users').on('click','.delete-user--btn', function(e) {
     e.preventDefault
-    var user_parent = $(this).parent();
-    var user = user_parent.find('a');
-    var user_id = user.data('user-id');
-    deleteUserId(user_id,user_parent);
+    var user = $(this).parent();
+    deleteUserId(user);
   })
 });
