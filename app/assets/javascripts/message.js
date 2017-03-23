24b794c479db9c149175208c078799ea6dbc2324
大高俊輝 <to.so.lo.1205@gmail.com>
2017-03-24 02:03:57 +0900

    modify message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,28 +1,29 @@
 $(document).on('turbolinks:load', function() {
   function buildHTML(message) {
-    var html = `<li class="chat-message">
-                  <div class="chat-message__header">
-                    <p class="chat-message__user">
-                      ${message.name}
-                    </p>
-                    <p class="chat-message__date">
-                      ${message.created_at}
-                    </p>
-                  </div>
-                  <div class="chat-message__text">
-                    ${message.body}
-                  </div>
-                </li>`
+    var html = 
+    `<li class="chat-message">
+      <div class="chat-message__header">
+        <p class="chat-message__user">
+          ${message.name}
+        </p>
+        <p class="chat-message__date">
+          ${message.created_at}
+        </p>
+      </div>
+      <p class="chat-message__text">
+        ${message.body}
+      </p>
+    </li>`
     $('.chat-messages').append(html);
   }
 
-  $('.chat-group-form__action-btn').on('submit', function(e) {
+  $('#chat-message-form__submit').on('click', function(e) {
     e.preventDefault();
     var textField = $('.js-form__text-field');
     var input = textField.val();
     $.ajax({
       type: 'POST',
-      url: './messages',
+      url: './messages.json',
       data: {
         message: {
           body: input
@@ -31,7 +32,6 @@ $(document).on('turbolinks:load', function() {
       dataType: 'json'
     })
     .done(function(data) {
-      console.log(data);
       buildHTML(data);
       textField.val('');
     })
