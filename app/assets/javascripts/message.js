3c2e600d63ce08d98a4533554a3f425ebceffe43
大高俊輝 <to.so.lo.1205@gmail.com>
2017-04-02 18:54:50 +0900

    revise the function of auto loading

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -6,14 +6,14 @@ $(document).on('turbolinks:load', function() {
   $('#file-input').on('change', function() {
     ajaxMessage();
   });
-  if(location.pathname.match("messages")) {
-    setInterval(autoLoading, 1000);
-  }
+  pageLoad();
 });
 
+var lastMessageId;
+
 function buildHTML(message) {
-  var image = message.image ? image = `<img src="${message.image}">` : image = '';
-  var html  = `<li class="chat-message">
+  var image = message.image ? `<img src="${message.image}">` : '' ;
+  var html  = `<li class="chat-message" data-id="${message.id}">
                  <div class="chat-message__header">
                    <p class="chat-message__user">
                      ${message.name}
@@ -36,48 +36,53 @@ function scroll() {
     });
 }
 
+function pageLoad() {
+  if(window.location.href.match(/messages/)) {
+    setInterval(autoLoading, 1000);
+  }
+}
+
 function ajaxMessage() {
   var formData = new FormData($('#new_message').get(0));
-  $.ajax({
-    type: 'POST',
-    url: './messages',
-    data: formData,
-    processData: false,
-    contentType: false,
-    dataType: 'json'
-  })
-  .done(function(data) {
-    buildHTML(data);
-    $('.js-form__text-field').val('');
-    $('#new_message').get(0).reset();
-    scroll();
-  })
-  .fail(function() {
-    alert('error');
-  });
+  if($('#message_body').val() || $('#file-input').val()){
+    $.ajax({
+      type: 'POST',
+      url: './messages',
+      data: formData,
+      processData: false,
+      contentType: false,
+      dataType: 'json'
+    })
+    .done(function(data) {
+      buildHTML(data);
+      $('.js-form__text-field').val('');
+      $('#new_message').get(0).reset();
+      lastMessageId = data.id
+      scroll();
+    })
+    .fail(function() {
+      alert('error');
+    });
+  }
 }
 
 function autoLoading() {
-  var id = location.href.split("/")[4];
+  lastMessageId = $('.chat-message').last().data('id');
   $.ajax({
     type: 'GET',
     url: './messages',
     data: {
-      group_id: id
+      id: lastMessageId
     },
     dataType: 'json'
   })
   .done(function(data) {
-    var messageNum = $('.chat-message').length;
-    var loadedMessageNum = data.length;
-    if(messageNum != loadedMessageNum) {
-      for(var i = messageNum; i < loadedMessageNum; i ++) {
-        buildHTML(data[messageNum]);
+    for(var i = 0; i < data.length; i ++) {
+      if(lastMessageId != data[i].id) {
+        buildHTML(data[i]);
         scroll();
       }
     }
   })
-  .fail(function() {
-    alert("ロードエラー");
-  });
+  .fail(function() {});
 }
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,15 @@
 class MessagesController < ApplicationController
 
   def index
-    @group    = Group.find(params[:group_id])
-    @groups   = current_user.groups.order(created_at: :DESC)
-    @members  = @group.users
-    @message  = current_user.messages.new
+    @group = Group.find(params[:group_id])
+    @groups = current_user.groups.order(created_at: :DESC)
+    @members = @group.users
+    @message = current_user.messages.new
     @messages = @group.messages
+    @newMessages = []
+    @messages.find_each(start: params[:id]) do |message|
+      @newMessages << message
+    end
     respond_to do |format|
       format.html
       format.json
