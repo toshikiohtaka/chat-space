6116695997fdbf4dabb4fa3c1578f0723e76183a
大高俊輝 <to.so.lo.1205@gmail.com>
2017-04-10 19:25:35 +0900

    refactor

---
+++ app/assets/javascripts/group.js
@@ -0,0 +1,69 @@
+$(document).on('turbolinks:load', function () {
+
+  var user_ids = [];
+
+  function addUser(user) {
+    var html = `<li class="chat-group-user" data-user-id="` + user.id + `" data-user-name="` + user.name + `">` + user.name +
+                  `<a class="user-btn--add"> 追加` + `</a>` +
+                `</li>`;
+    $('#chat-group-members').append(html);
+  }
+
+  function addUserToGroup(user) {
+    var name = user.data('user-name');
+    var id   = user.data('user-id');
+    var html =  `<li class="chat-group-added-user">` + name +
+                  `<input name="group[user_ids][]" value="` + id + `type="hidden">` +
+                  `<a class="user-btn--delete"> 削除` + `</a>` +
+                `</li>`;
+    $('#chat-group-users').append(html);
+    user_ids.push(id);
+  }
+
+  function deleteUserId(user) {
+    var id  = user.data('user-id');
+    var num = user_ids.indexOf(id);
+    user_ids.splice(num,1);
+    user.remove();
+  }
+
+  $('#chat_group_names').on('keyup', function() {
+    var preWord = '';
+    var input   = $('#chat_group_names').val();
+    if(input == preWord) {
+      $('.chat-group-user').remove();
+    } else {
+      $('.chat-group-user').remove();
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: {
+          user: input
+        },
+        dataType: 'json'
+      })
+      .done(function(data) {
+        $.each(data, function(i,user) {
+          if(user_ids.indexOf(user.id) == -1) {
+            addUser(user);
+          }
+        });
+      })
+      .fail(function() {
+        alert('error');
+      });
+    }
+  });
+  $('#chat-group-members').on('click','.user-btn--add',function() {
+    var user = $(this).parent();
+    var id   = user.data('user_id');
+    if(user_ids.indexOf(id) == -1) {
+      user.remove();
+      addUserToGroup(user);
+    }
+  });
+  $('#chat-group-users').on('click','.user-btn--delete', function() {
+    var user = $(this).parent();
+    deleteUserId(user);
+  })
+});
---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,77 @@
+var lastMessageId;
+
+function buildHTML(message) {
+  var image = message.image ? `<img src="` + message.image + `">` : '' ;
+  var html  = `<li class="chat-message" data-id="` + message.id + `">` +
+                 `<div class="chat-message__header">` +
+                   `<p class="chat-message__user">` +
+                     message.name +
+                   `</p>` +
+                   `<p class="chat-message__date">` +
+                     message.created_at +
+                   `</p>` +
+                 `</div>` +
+                 `<p class="chat-message__text">` +
+                   message.body +
+                 `</p>` +
+                 image +
+               `</li>`;
+  $('.chat-messages').append(html);
+}
+
+function scroll() {
+  $('.chat-body').animate({
+      scrollTop:$('.chat-messages').height()
+    });
+}
+
+function autoLoading() {
+  lastMessageId = $('.chat-message').last().data('id');
+  $.ajax({
+    type: 'GET',
+    url: './messages',
+    data: {
+      id: lastMessageId
+    },
+    dataType: 'json'
+  })
+  .done(function(data) {
+    $.each(data, function(i, message) {
+      buildHTML(message);
+      scroll();
+    });
+  });
+}
+
+function pageLoad() {
+  if(window.location.href.match(/messages/)) {
+    setInterval(autoLoading, 1000);
+  }
+}
+
+$(document).on('turbolinks:load', function() {
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData($(this).get(0));
+    if($('#message_body').val() || $('#file-input').val()){
+      $.ajax({
+        type: 'POST',
+        url: $(this).attr('action'),
+        data: formData,
+        processData: false,
+        contentType: false,
+        dataType: 'json'
+      })
+      .done(function(data) {
+        buildHTML(data);
+        scroll();
+      })
+      .fail(function() {
+        alert('error');
+      });
+    }
+    $(this).get(0).reset();
+    return false;
+  });
+  pageLoad();
+});
