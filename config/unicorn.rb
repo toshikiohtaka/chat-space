1d40d6f6fc32fc5d97435c8bba2795f1248e54c3
大高俊輝 <to.so.lo.1205@gmail.com>
2017-04-13 17:38:07 +0900

    edit config/unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,41 +1,74 @@
-app_path = File.expand_path('../../', __FILE__)
+# app_path = File.expand_path('../../', __FILE__)
 
-worker_processes 1
+# worker_processes 1
 
-working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+# working_directory app_path
 
-listen 3000
-timeout 60
+# pid "#{app_path}/tmp/pids/unicorn.pid"
+# stderr_path "#{app_path}/log/unicorn.stderr.log"
+# stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+# listen 3000
+# timeout 60
+
+# preload_app true
+# GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+# check_client_connection false
+
+# run_once = true
+
+# before_fork do |server, worker|
+#   defined?(ActiveRecord::Base) &&
+#     ActiveRecord::Base.connection.disconnect!
+
+#   if run_once
+#     run_once = false # prevent from firing again
+#   end
+
+#   old_pid = "#{server.config[:pid]}.oldbin"
+#   if File.exist?(old_pid) && server.pid != old_pid
+#     begin
+#       sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+#       Process.kill(sig, File.read(old_pid).to_i)
+#     rescue Errno::ENOENT, Errno::ESRCH => e
+#       logger.error e
+#     end
+#   end
+# end
+
+# after_fork do |_server, _worker|
+#   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+# end
+
+$worker = 1
+$timeout = 30
+$app_dir = '/var/www/chat-space/current'
+$listen = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
+$pid = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$std_log = File.expand_path 'log/unicorn.log', $app_dir
+
+worker_processes  $worker
+working_directory $app_dir
+stderr_path $std_log
+stdout_path $std_log
+timeout $timeout
+listen  $listen
+pid $pid
 
 preload_app true
-GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
-
-check_client_connection false
-
-run_once = true
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
-
-  if run_once
-    run_once = false # prevent from firing again
-  end
-
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && server.pid != old_pid
+  if old_pid != server.pid
     begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH => e
-      logger.error e
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
     end
   end
 end
 
-after_fork do |_server, _worker|
-  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
